0ad013b103c7f5c7ff272ae5cd5e295d396f8771
Artur <explosionggg@mail.ru>
2019-11-05 19:54:16 +0300

    added routing, divided MainLayout into components

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      :to="link.url"
+      :exact="link.exact"
+      tag="li"
+      active-class="active"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['isOpen'],
+  data: () => ({
+    links: [
+      { url: '/', title: 'Счет', exact: true },
+      { url: '/history', title: 'История', exact: false },
+      { url: '/planning', title: 'Планирование', exact: false },
+      { url: '/record', title: 'Новая запись', exact: false },
+      { url: '/categories', title: 'Категории', exact: false }
+    ]
+  })
+}
+</script>
